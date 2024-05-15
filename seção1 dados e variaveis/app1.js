6e243e30dc5818a34f4236a9b58af7e4d196dc97
Dominique Albuquerque <domborgesbr2468@gmail.com>
2024-05-15 15:06:43 -0300

    estudo js app1

---
+++ seção1 dados e variaveis/app1.js
@@ -0,0 +1,23 @@
+/* Tipos de Dados e Variaveis */
+
+/*
+Sistema de Cadastro
+*/
+
+let nome = "Dom"; //String
+let sobrenome = "Dq";
+let idade = 22; // Number
+const numCliente = "63992";
+let valorEmprestimo = 15000;
+let taxaDeJuros = 10;
+let eBomPagador = "sim";
+eBomPagador = true; // Boolean
+console.log("olá sr. " + nome, "sua idade e " + idade, "anos");
+
+/* Desafios 1 */
+let Nome = "toddy";
+let preço = 25;
+let validade = true;
+console.log(Nome);
+console.log(preço);
+console.log(validade);
