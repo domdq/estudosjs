d3a3bac90f9e576701aef655fdfd80646ea3bc63
Dominique Albuquerque <domborgesbr2468@gmail.com>
2024-05-16 17:15:47 -0300

    app2 js

---
+++ seção2 operadores/app2.js
@@ -0,0 +1,53 @@
+/// Operadores ///
+
+/*
+ Adição +
+ subtração -
+ Multiplicação *
+ Divisão /
+ Exponenciação **
+ modulo %
+ */
+
+/// Operadores de comparação ///
+
+/*
+< menor que
+> maior que
+<= mrnor igual que
+>= maior igual que
+== igual a
+=== exatamente igual
+!= diferente
+!== tipos difrente
+*/
+
+/// operadores logicos ///
+
+/*
+! não
+|| ou 
+&& e
+*/
+
+/// Desafio ///
+let bim1 = 6.5;
+let bim2 = 7.0;
+let bim3 = 8.0;
+let bim4 = 6.5;
+let soma = (bim1 + bim2 + bim3 + bim4) / 4;
+console.log(soma, "" + "aluno foi aprovado");
+
+///sistema banco com operadores///
+let nome = "Dom";
+let sobrenome = "Dq";
+let idade = 22;
+const numCliente = 63992;
+let valorEmprestimo = 15000;
+let taxaDeJuros = 0.1;
+let eBomPagador = true;
+let numAnos = 6;
+
+let juros = valorEmprestimo * taxaDeJuros * numAnos;
+let montante = valorEmprestimo + juros;
+console.log("valor do emprestimo: " + montante);
