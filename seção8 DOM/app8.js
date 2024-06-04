cbe15ff0b3acdc5be12b3f9ec90cfa5fecf43b4d
domdq <domborgesbr2468@gmail.com>
2024-06-04 15:15:21 -0300

    EstudoJS

---
+++ seção8 DOM/app8.js
@@ -0,0 +1,22 @@
+/***** DOM *****/
+
+// Métodos de acesso
+/*
+  getElementByID()
+ getElementByClassName()
+ getElementBytagName()
+ querySelector()
+ */
+
+/*******  getElementByID() *******/
+// elemento = document.getElementById("id")
+const paleta1 = document.getElementById("paleta1");
+const paleta2 = document.getElementById("paleta2");
+const paleta3 = document.getElementById("paleta3");
+
+/******** getElementByClassName() *******/
+// elemento = document.getElementByClassName("class")
+const classPaleta = document.getElementsByClassName();
+
+/******* getElementByClassName() *******/
+// elemento = document.getElementTagName("tagname")
